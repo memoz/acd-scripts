1666a882aaf84e2d0ea69692d99787d46332b426
Bowen Jiang <unctas@gmail.com>
2016-04-14 01:55:06 +0800

    Update batch.sh Directory list is now generated in a "case" statement which accepts: 1. -l as the first argument to indicate the second argument is a file containing a list of directories. 2. Empty argument. Interpreted as all directories in the current path. 3. Arguments as directories to include.

--- batch.sh
+++ batch.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
-#
-# Copyright (C) 2015  Bowen Jiang  (unctas@gmail.com)
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # 获取脚本路径
 # Obtain script location
@@ -35,16 +20,22 @@ check_exit_code $? "Check mailx"
 
 # 获取待打包目录列表
 # Obtain directory list
-if [ -z "${1}" ]; then
-    for file in *; do
-        if [[ -d "${file}" && ! -L "${file}" ]]; then
-            dir_list=("${dir_list[@]}" "${file}")
-        fi
-    done
-else
-    mapfile -t dir_list < "${1}"
-    check_exit_code $? "Mapfile"
-fi
+case "${1}" in
+    -l)
+       mapfile -t dir_list < "${2}"
+       check_exit_code $? "Mapfile"
+       ;;
+    "")
+       for file in *; do
+           if [[ -d "${file}" && ! -L "${file}" ]]; then
+               dir_list+=("${file}")
+           fi
+       done
+       ;;
+    *)
+       dir_list=("${@}")
+       ;;
+esac
 
 # 向用户确认待打包目录
 # Confirm directory list with user
